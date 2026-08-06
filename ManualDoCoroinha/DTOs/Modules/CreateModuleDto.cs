648b888220082fff133f0345231db4ef959e5783
Andrey Cavalcante <andrey.cavalcante06@gmail.com>
2026-08-06 19:18:53 -0300

    refactor: corrige moduledto

--- ManualDoCoroinha/DTOs/Modules/CreateModuleDto.cs
+++ ManualDoCoroinha/DTOs/Modules/CreateModuleDto.cs
@@ -1,5 +1,6 @@
 ﻿using ManualDoCoroinha.DTOs.Lessons;
 using ManualDoCoroinha.DTOs.Quizzes;
+using ManualDoCoroinha.Enums;
 
 namespace ManualDoCoroinha.DTOs.Modules;
 
@@ -8,6 +9,7 @@ public class CreateModuleDto
     public string Title { get; set; }
     public string Description { get; set; }
     public int Order { get; set; }
+    public ModuleCategory Category { get; set; }
     public bool IsActive { get; set; }
     public Guid? PrerequisiteId { get; set; }
 }
